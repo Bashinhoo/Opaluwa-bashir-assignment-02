65dae722ac94ad974c42d0f1d3940f09f4f81015
Opaluwa Bashir <opaluwabashir10@gmail.com>
2022-07-21 11:49:11 +0000

    challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
+  let sumOfNumbers = 0
+  for (let i=0; i< arrayOfNumbers.length; i++){
+    sumOfNumbers+ = arrayOfNumbers [i]
+  }
     return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
+  let countEvenNumbers = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++){
+    if (arrayOfNumbers[i] % 2 === 0) {
+      countEvenNumbers++;
+    };
+  };
     return arrayOfNumbers.length;
-}
-// assignment.countEvenNumbers = countEvenNumbers;
+};
+ countEvenNumbers(arrayOfNumbers);
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
